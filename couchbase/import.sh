b823f92de88de496fdd9a791ec11b873d9a3288c
Sergey Avseyev <sergey.avseyev@gmail.com>
2013-06-17 17:37:11 +0300

    Add minified and stripped versions in JS

--- couchbase/import.sh
+++ couchbase/import.sh
@@ -12,3 +12,12 @@ static const char *MSGPACK_FUNCTION_STRING =
 .
   :write!
 EOF
+
+uglifyjs --beautify -o msgpack.codec.stripped.js msgpack.codec.js
+vim -u NONE -U NONE -E -s msgpack.codec.stripped.js <<EOF
+  :%s/^\n//g
+  :%s/^/    /g
+  :write!
+EOF
+
+uglifyjs --compress --mangle -o msgpack.codec.min.js msgpack.codec.js
